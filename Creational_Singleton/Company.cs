49f0bfc6651ee2c70713432f95e7c84902305ec3
BGuney <beytullahguney@gmail.com>
2020-03-29 16:27:46 +0300

    Singleton Pattern - Company etc

---
+++ Creational_Singleton/Company.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Creational_Singleton
+{
+    public sealed class Company
+    {
+        private static int counter = 0;
+        private static Company instance = null;
+
+        public static Company GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    if (true)
+                    {
+                        instance = new Company();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        private Company()
+        {
+            counter++;
+            Console.WriteLine(string.Format("Counter Value : {0}", counter));
+        }
+
+        public void PrintDetails(string Message)
+        {
+            Console.WriteLine(Message);
+        }
+
+    }
+}
